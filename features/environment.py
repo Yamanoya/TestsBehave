1867c0b6b659aaae941f29cb8e34955d9c04c930
Ильгиз Юсупов <YusupovIK@astralnalog.ru>
2022-06-15 18:32:53 +0500

    fix: Исправить запуск тестов в докере

--- features/environment.py
+++ features/environment.py
@@ -13,10 +13,15 @@ def browser_chrome(context: Context):
     # создаем объект класа ChromeOptions для добавления параметров при запуске хрома.
     # Каждый параметр можно погуглить :)
     options = webdriver.ChromeOptions()
-    options.add_argument('no-sandbox')
-    options.add_argument('isable-extensions')
     options.add_argument('start-maximized')
-    # options.add_argument('headless')
+    options.add_argument('disable-infobars')
+    options.add_argument('headless')
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--start-maximized')
+    options.add_argument('--disable-dev-shm-usage')
+    
     # Обявляем экземпляр класа хромдрайвера ChromeDriverManager класс для автоматического скачивания драйвера
     # В будущем соответственно вызываем браузер через контекст как показано ниже.
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
